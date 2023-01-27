8c0f7453b8acd92bf81d72669fb178020f7c3e10
sjudicael <116107756+sjudicael@users.noreply.github.com>
2023-01-27 12:07:26 +0100

    page

--- index.php
+++ index.php
@@ -3,16 +3,7 @@
 <title>Test PHP</title>
 </head>
 <body>
-<?php echo '<p>Bonjour le monde</p>'; 
-
- <form action="#" method="post">
- <p>Votre nom : <input type="text" name="nom" /></p>
- <p>Votre âge : <input type="text" name="age" /></p>
- <p><input type="submit" value="OK"></p>
-
- </form>
-
-?>
+<?php echo '<p>Bonjour le monde</p>'; ?>
 
 
 </form>
